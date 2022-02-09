70e79159de7f26d7ca8e2cdfce1ff346aca50188
Lucassoares10 <ls6359575@gmail.com>
2022-02-09 11:36:07 -0300

    Criado style para o background do botao, utilizando a interface dentro da constante, e aplicado os valores para cada ação

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
-    import Modal from 'react-modal';
+    import { useState } from 'react';
+import Modal from 'react-modal';
     import closeImg from '../../assets/close.svg';
     import incomeImg from '../../assets/income.svg';
     import outcomeImg from '../../assets/outcome.svg';
-    import { Container,TransactionTypeContainer } from './styles';
+    import { Container,TransactionTypeContainer, RadioBox } from './styles';
 
     interface newTransactionModalProps {
         isOpen: boolean;
@@ -10,6 +11,8 @@
     }
 
     export function NewTransactionModal({isOpen, onRequestClose} :newTransactionModalProps) {
+        const [type, setType] = useState('deposit');
+
         return (
         <Modal isOpen={isOpen}
                 onRequestClose={onRequestClose}
@@ -31,15 +34,24 @@
                 <input type="number" placeholder="Valor"/>
 
                 <TransactionTypeContainer >
-                    <button type="button">
+                    <RadioBox type="button"
+                        onClick={() => {setType('deposit'); }}
+                        isActive ={type == 'deposit'}
+                        activeColor ="green"
+                        >
+
                         <img src={incomeImg} alt="Entrada"/>
                         <span>Entrada</span>
-                    </button>
+                    </RadioBox>
                     
-                    <button type="button">
+                    <RadioBox type="button"
+                    onClick={() => {setType('widthdraw'); }}
+                    isActive ={type == 'widthdraw'}
+                    activeColor ="red"
+                    >
                         <img src={outcomeImg} alt="Saida"/>
                         <span>Saida</span>
-                    </button>
+                    </RadioBox>
                     
                 </TransactionTypeContainer>
                 
